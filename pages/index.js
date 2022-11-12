e14c1396b2b8e7220c7bc6268f11439b88ebcdfd
Abhishek <abhishekparmar162@gmail.com>
2022-11-13 00:44:47 +0530

    started product page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = () => {
    <ul className='text-white cursor-pointer bg-cyan-800 text-4xl'>
    <li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
 <li className='p-4 font-bold'>    <Link href='/price' >Price</Link> </li>
-<li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
+<li className='p-4 font-bold'> <Link href='/product' >Product</Link> </li>
 <li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
 <li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
     </ul>   
