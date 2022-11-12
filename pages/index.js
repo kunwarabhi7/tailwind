679112b0663fac4078eae524993c65153d0ac3aa
Abhishek <abhishekparmar162@gmail.com>
2022-11-12 23:41:19 +0530

    added pricing page

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,11 @@ const Home = () => {
     <>
     <div className='bg-black text-white text-center text-7xl py-4 '>Tailwind</div>
    <ul className='text-white cursor-pointer bg-cyan-800 text-4xl'>
-    <Link href='/email' className='p-8 font-bold'>Email</Link>
-    <li className='p-8 font-bold'>Email</li>
-    <li className='p-8 font-bold'>Email</li>
-    <li className='p-8 font-bold'>Email</li>
+   <li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
+<li className='p-4 font-bold'>    <Link href='/price' >Price</Link> </li>
+<li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
+<li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
+<li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
     </ul>   
     </>
   )
---
+++ pages/price.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const price = () => {
+  return (
+    <div>
+      price
+    </div>
+  )
+}
+
+export default price
