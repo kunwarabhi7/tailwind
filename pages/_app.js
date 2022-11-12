0980bda38c6685b7d15f7dae9e66ad569a02ed79
Abhishek <abhishekparmar162@gmail.com>
2022-11-13 00:48:30 +0530

    added navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Tailwind app created by Abhishek" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+<Link href='/'>      <div className='bg-black text-white text-center text-7xl py-4 '>Tailwind</div> </Link>
 <Component {...pageProps} />
 </>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import Link from 'next/link'
 const Home = () => {
   return (
     <>
-    <div className='bg-black text-white text-center text-7xl py-4 '>Tailwind</div>
-   <ul className='text-white cursor-pointer bg-cyan-800 text-4xl'>
+   <ul className='text-white cursor-pointer bg-cyan-800 text-4xl h-screen'>
    <li className='p-4 font-bold'> <Link href='/email' >Email</Link> </li>
 <li className='p-4 font-bold'>    <Link href='/price' >Price</Link> </li>
 <li className='p-4 font-bold'> <Link href='/product' >Product</Link> </li>
