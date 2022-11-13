15dc880ae8139e29a3309ac8f5bdb0852f9f2612
Abhishek <abhishekparmar162@gmail.com>
2022-11-13 16:29:38 +0530

    done page

--- pages/product.jsx
+++ pages/product.jsx
@@ -5,16 +5,16 @@ import Heart from '../public/image/heart.png'
 const product = () => {
   return (
     <div className='flex justify-center items-center h-screen bg-gray-200'>
-      <div className='bg-white w-[500px] h-[440px] flex shadow-lg shadow-white'>
+      <div className='bg-white w-[600px]  h-[440px] flex shadow-lg shadow-white'>
         <div className='w-64 h-full'>
           <Image src={Headphone} width='200' height='200' className='h-48 p-8 w-60' />
         </div>
-        <div className='m-1'>
+        <div className='m-1 mr-4'>
           <button className='text-white bg-black rounded-3xl mt-8 p-2'>Free Shipping</button>
           <p className='text-xl font-bold'>Razer Kraken Kitty Edt Gaming Headset Quartz</p>
           <p className='font-bold line-through ' >$700</p>
-          <p className='font-bold text-4xl'>$499</p>
-          <p>The offer is valid untill April 3 or as long as stock lasts!</p>
+          <p className='font-bold text-4xl mt-4'>$499</p>
+          <p className='text-xs text-gray-400'>The offer is valid untill April 3 or as long as stock lasts!</p>
           <button className='w-full bg-blue-500 text-white py-2 px-5 rounded-xl mt-2'>Add to Cart</button>
           <h1><span className='text-green-500 text-4xl'> &#x2022; </span> 50+ pcs. in stock.</h1>
           <div className='flex w-full h-12 mt-4'>
