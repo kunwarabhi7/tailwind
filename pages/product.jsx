56cb0932aa3d262dc31e2fa9f38c80d25d5554fe
Abhishek <abhishekparmar162@gmail.com>
2022-11-13 15:57:59 +0530

    added logo

--- pages/product.jsx
+++ pages/product.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from 'next/image'
+import Headphone from '../public/image/headphone.png'
 
 const product = () => {
   return (
-    <div>
-      Product
+    <div className='flex justify-center items-center h-screen bg-gray-500'>
+      <div className='bg-white w-[500px] h-96 flex'>
+        <div className='w-64 h-full'>
+          <Image src={Headphone} width='200' height='200' className='h-48 p-8 w-60' />
+        </div>
+        <div className='m-1'>
+          <button className='text-white bg-black rounded-3xl mt-8 p-2'>Free Shipping</button>
+          <p className='text-xl font-bold'>Razer Kraken Kitty Edt Gaming Headset Quartz</p>
+        </div>
+
+      </div>
     </div>
   )
 }
