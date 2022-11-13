03cededb9661a63262db92fd25acc49a5128ce1c
Abhishek <abhishekparmar162@gmail.com>
2022-11-13 16:34:15 +0530

    add hover effect

--- pages/product.jsx
+++ pages/product.jsx
@@ -7,7 +7,7 @@ const product = () => {
     <div className='flex justify-center items-center h-screen bg-gray-200'>
       <div className='bg-white w-[600px]  h-[440px] flex shadow-lg shadow-white'>
         <div className='w-64 h-full'>
-          <Image src={Headphone} width='200' height='200' className='h-48 p-8 w-60' />
+          <Image src={Headphone} width='200' height='200' className='h-48 transform hover:scale-110 duration-500 p-8 w-60' />
         </div>
         <div className='m-1 mr-4'>
           <button className='text-white bg-black rounded-3xl mt-8 p-2'>Free Shipping</button>
@@ -15,14 +15,14 @@ const product = () => {
           <p className='font-bold line-through ' >$700</p>
           <p className='font-bold text-4xl mt-4'>$499</p>
           <p className='text-xs text-gray-400'>The offer is valid untill April 3 or as long as stock lasts!</p>
-          <button className='w-full bg-blue-500 text-white py-2 px-5 rounded-xl mt-2'>Add to Cart</button>
+          <button className='w-full bg-blue-500 text-white py-2 px-5 rounded-xl mt-2 transform hover:scale-105 duration-300 cursor-pointer'>Add to Cart</button>
           <h1><span className='text-green-500 text-4xl'> &#x2022; </span> 50+ pcs. in stock.</h1>
           <div className='flex w-full h-12 mt-4'>
-            <div className='flex justify-between border w-32 p-4 h-full border-gray-400'>
+            <div className='flex justify-between border transform hover:scale-105 duration-300 cursor-pointer w-32 p-4 h-full border-gray-400'>
               <Image src={Weight} className='h-4 w-4 ' />
               <p className='text-xs '>Add to Cart</p>
             </div>
-            <div className='flex justify-between border w-32 p-4 h-full ml-8 border-gray-400'>
+            <div className='flex justify-between transform hover:scale-105 duration-300 cursor-pointer border w-32 p-4 h-full ml-8 border-gray-400'>
               <Image src={Heart} className='h-4 w-4 ' />
               <p className='text-xs '>Add to wishlist</p>
             </div>
