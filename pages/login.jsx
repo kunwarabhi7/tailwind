3005e97dac8e066786414c936bc8ccd4831e3111
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 14:37:50 +0530

    fix screen ratio

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,10 +3,10 @@ import bgImage from '../public/image/image.jpg'
 const login = () => {
   return (
     <div className='bg-rose-50 flex justify-center item-center min-h-screen '>
-<div className='relative shadow-2xl flex flex-row justify-between bg-white m-20 w-[30rem] '>
+<div className='relative shadow-2xl flex flex-row justify-between bg-white my-48 mx-32 w-[90rem] '>
 <div className='m-6'>
-    <h1 className=' pt-4 mb-4 text-3xl font-bold'>Log In</h1>
-    <p>Log in to your account to upload or download picture , music or video .</p>
+    <h1 className=' pt-4 mb-12 text-7xl font-bold'>Log In</h1>
+    <p className='text-gray-400 text-xl mb-4'>Log in to your account to upload or download picture , music or video .</p>
     <input type="text" className=' border-gray-200 border p-3 pr-12' placeholder='Enter your email address' />
     <div>
         <button>Forget Password</button>
@@ -18,7 +18,7 @@ const login = () => {
         <button>Google</button>
     </div>
 </div>
-<div className='w-64 h-screen'>
+<div className='w-[28rem] rounded-xl'>
     <Image src={bgImage} />
 </div>
 </div>
