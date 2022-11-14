17c497b7b271821f6cf39bbb80f2c0bc48744443
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 14:30:22 +0530

    setup heading

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,9 +3,9 @@ import bgImage from '../public/image/image.jpg'
 const login = () => {
   return (
     <div className='bg-rose-50 flex justify-center item-center min-h-screen '>
-<div className='relative flex flex-row justify-between bg-white m-20 w-[30rem] '>
-<div>
-    <h1 className='text-center pt-4 pl-12 text-3xl font-bold'>Log In</h1>
+<div className='relative shadow-2xl flex flex-row justify-between bg-white m-20 w-[30rem] '>
+<div className='m-6'>
+    <h1 className=' pt-4 mb-4 text-3xl font-bold'>Log In</h1>
     <p>Log in to your account to upload or download picture , music or video .</p>
     <input type="text" className=' border-gray-200 border p-3 pr-12' placeholder='Enter your email address' />
     <div>
