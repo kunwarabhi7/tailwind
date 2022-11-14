da295352a9f2fc84b2d769d70d06641a2ca74290
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 15:48:57 +0530

    added header

--- pages/clipper.jsx
+++ pages/clipper.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Image from 'next/image'
+import Header from '../public/image/Clipper/bg-header-desktop.png'
+import HeaderMb from '../public/image/Clipper/bg-header-mobile.png'
+import Logo from '../public/image/Clipper/logo.svg'
 
 const clipper = () => {
   return (
-    <div>
-      Clipper
+    <div className='relative'>
+<Image src={Header} className='w-full hidden md:block' />
+<Image src={HeaderMb} className='block md:hidden w-full' />
+<Image src={Logo} className='absolute top-10 left-[45%]  items-center flex' />
     </div>
   )
 }
