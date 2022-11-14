802c1dc3eb39784c096c1f4fcaa0934f91fd6fd6
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 16:02:40 +0530

    added button

--- pages/clipper.jsx
+++ pages/clipper.jsx
@@ -9,7 +9,17 @@ const clipper = () => {
 <Image src={Header} className='w-full hidden md:block' />
 <Image src={HeaderMb} className='block md:hidden w-full' />
 <Image src={Logo} className='absolute top-10 left-[45%]  items-center flex' />
+<div>
+    <p>A history of everything you copy</p>
+    <p>Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on your devices.</p>
+<div className='flex justify-evenly'>
+    <button className='bg-green-500 text-white py-2 px-3'>Download for iOS</button>
+    <button className='bg-blue-800 text-white py-2 px-3'>Download for Mac</button>
+</div>
+</div>
+
     </div>
+
   )
 }
 
