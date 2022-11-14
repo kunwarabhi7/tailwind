1995d57269bdbe193f524206eef8d1fa4858f257
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 23:28:30 +0530

    fix first heading

--- pages/clipper.jsx
+++ pages/clipper.jsx
@@ -20,12 +20,12 @@ import VG from '../public/image/Clipper/logo-vector-graphics.png'
 const clipper = () => {
   return (
     <div className='relative'>
-<Image src={Header} className='w-full hidden md:block' />
+<Image src={Header} className='w-full hidden md:block h-64' />
 <Image src={HeaderMb} className='block md:hidden w-full' />
 <Image src={Logo} className='absolute top-10 left-[45%]  items-center flex' />
-<div>
-    <p>A history of everything you copy</p>
-    <p>Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on your devices.</p>
+<div className='text-center'>
+    <p className='text-5xl font-bold'>A history of everything you copy.</p>
+    <p className='py-4 text-gray-400'>Clipboard allows you to track and organize everything you copy.<br/> Instantly access your clipboard on your devices.</p>
 <div className='flex justify-evenly'>
     <button className='bg-green-500 text-white py-2 px-3'>Download for iOS</button>
     <button className='bg-blue-800 text-white py-2 px-3'>Download for Mac</button>
