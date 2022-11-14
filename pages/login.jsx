07a91fe850a7d6354e74a32f0163baa9f210a930
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 14:27:32 +0530

    setup project

--- pages/login.jsx
+++ pages/login.jsx
@@ -7,6 +7,16 @@ const login = () => {
 <div>
     <h1 className='text-center pt-4 pl-12 text-3xl font-bold'>Log In</h1>
     <p>Log in to your account to upload or download picture , music or video .</p>
+    <input type="text" className=' border-gray-200 border p-3 pr-12' placeholder='Enter your email address' />
+    <div>
+        <button>Forget Password</button>
+        <button>Next 	&#8594;</button>
+    </div>
+    <button>Or log in with</button>
+    <div>
+        <button>Facebook</button>
+        <button>Google</button>
+    </div>
 </div>
 <div className='w-64 h-screen'>
     <Image src={bgImage} />
