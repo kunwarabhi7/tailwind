0090cadfd990e7939867672251fc45abcfac5801
Abhishek <abhishekparmar162@gmail.com>
2022-11-14 14:42:38 +0530

    fix buttons

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,13 +4,13 @@ const login = () => {
   return (
     <div className='bg-rose-50 flex justify-center item-center min-h-screen '>
 <div className='relative shadow-2xl flex flex-row justify-between bg-white my-48 mx-32 w-[90rem] '>
-<div className='m-6'>
+<div className='m-8'>
     <h1 className=' pt-4 mb-12 text-7xl font-bold'>Log In</h1>
     <p className='text-gray-400 text-xl mb-4'>Log in to your account to upload or download picture , music or video .</p>
-    <input type="text" className=' border-gray-200 border p-3 pr-12' placeholder='Enter your email address' />
-    <div>
-        <button>Forget Password</button>
-        <button>Next 	&#8594;</button>
+    <input type="text" className=' mb-4 border-gray-200 border p-3 pr-12' placeholder='Enter your email address' />
+    <div className='flex justify-between'>
+        <button className='text-blue-400'>Forget Password</button>
+        <button className='text-white bg-cyan-500 rounded-lg p-5'>Next  <span className='font-bold text-2xl'>	&#8594; </span></button>
     </div>
     <button>Or log in with</button>
     <div>
