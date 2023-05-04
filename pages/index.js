eb6dccdc0b13d2d7161ea19a7ef43597a0d401a0
Abhishek <104254107+kunwarabhi7@users.noreply.github.com>
2023-05-04 17:32:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
 <li className='p-4 font-bold'>    <Link href='/price' >Price</Link> </li>
 <li className='p-4 font-bold'> <Link href='/product' >Product</Link> </li>
 <li className='p-4 font-bold'> <Link href='/image' >Image Gallery</Link> </li>
-<li className='p-4 font-bold'> <Link href='/login' >Login</Link> </li>
+<li className='p-4 font-bold'> <Link href='/login' >Loginn</Link> </li>
     </ul>   
     <ul className='text-white cursor-pointer bg-cyan-800 text-4xl h-screen'>
     <li className='p-4 font-bold'> <Link href='/clipper' >Clipper</Link> </li>
